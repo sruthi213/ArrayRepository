a4c9601fa8ec9c3bb45756f7508bd0b34a3b37d0
sruthi213 <srupremsundar@gmail.com>
2020-08-18 15:26:42 +0530

    child2 changes made

--- ArrayList/src/Com/cg/list/ArrayListCollectionSort.java
+++ ArrayList/src/Com/cg/list/ArrayListCollectionSort.java
@@ -8,6 +8,7 @@ package Com.cg.list;
 	        public static void main(String[] args) {
 	            List<Integer> numbers = new ArrayList<>();
 	            numbers.add(13);
+	            numbers.add(8);
 	            numbers.add(7);
 	            numbers.add(18);
 	            numbers.add(5);
