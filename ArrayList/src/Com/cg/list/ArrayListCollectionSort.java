81afe8393b1f26b3fa1faacf7f95ac3a9b43ec88
sruthi213 <srupremsundar@gmail.com>
2020-08-18 13:55:02 +0530

    My first commit

---
+++ ArrayList/src/Com/cg/list/ArrayListCollectionSort.java
@@ -0,0 +1,37 @@
+package Com.cg.list;
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.Iterator;
+	import java.util.List;
+	public class ArrayListCollectionSort {
+	//sorting the collection elements
+	        public static void main(String[] args) {
+	            List<Integer> numbers = new ArrayList<>();
+	            numbers.add(13);
+	            numbers.add(7);
+	            numbers.add(18);
+	            numbers.add(5);
+	            numbers.add(2);
+	            System.out.println("Before : " + numbers);
+	            // Sorting an ArrayList using Collections.sort() method
+	            Collections.sort(numbers);
+	            Iterator itr = numbers.iterator();
+	            while (itr.hasNext())
+	 	        {
+	                 Integer result = (Integer) itr.next();
+	                 
+	                 System.out.println(result);
+	            }
+	            System.out.println("After : " + numbers);
+	        }
+	    }
+
+
+
+
+
+
+
+
+
+
